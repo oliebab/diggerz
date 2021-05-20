99abbf49c0e5134fe6328bd6684be84559727d53
Olivier Babiard <olivier.babiard@gmail.com>
2021-05-20 14:55:29 +0200

    v2

--- routes/release.js
+++ routes/release.js
@@ -7,33 +7,22 @@ const uploader = require("./../config/cloudinary");
 /* GET playlist page. */
 router.get("/:id", async function (req, res, next) {
   const foundRelease = await ReleaseModel.findById(req.params.id);
+  // const foundUser = await UserModel.model
   console.log(foundRelease);
 
   res.render("release", {
     release: foundRelease,
   });
-}); //get play list by id
+}); 
 
 router.post("/",uploader.single("image"), async (req, res, next) => {
   const newRelease = {...req.body };
-  console.log(newRelease);
-  console.log(newRelease.youtubeLink);
-
-
+  
   const embedLink = newRelease.youtubeLink.replace("watch?v=","embed/");
   newRelease.youtubeLink = embedLink;
 
-  console.log(embedLink)
-
-
-
-  // const embedLink = youtubeLink.replace("watch?v=","embed/");
-  // newRelease.youtubeLink = embedLink;
-  
-
-  // modifier le lien
   newRelease.userId = req.session.currentUser._id 
-  // newRelease.youtubeLink = "toto" // mettre le nouveau lien;
+  
   try {
     await ReleaseModel.create(newRelease);
     res.redirect("/profile")
@@ -42,18 +31,4 @@ router.post("/",uploader.single("image"), async (req, res, next) => {
   }
 });
 
-// router.post("/release", async (req, res, next) => {
-//   const newRelease = { ...req.body };
-//  console.log(newRelease)
-//   try {
-//     await ReleaseModel.create(newRelease);
-//     res.redirect("/profile")
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-
-/* /playlist/{{playlist._id}}/release */
-//router.post("/playlist/:id/release")
 module.exports = router;
